e8e6c0b17c3ac6f5c22f9fe82257a19a96ba64b8
GNugger <wanfay.suen@gmail.com>
2023-01-09 11:04:44 +0100

    added some comments what the code does

--- lesson2 sthlm/high_low_solution_teacherscode.py
+++ lesson2 sthlm/high_low_solution_teacherscode.py
@@ -14,16 +14,16 @@ while True:
 
     guess = input("Vad är din gissning högre eller lägre? (h/l) (exit): ")
 
-    if guess != "h" and guess != "l" and guess != "exit":
+    if guess != "h" and guess != "l" and guess != "exit": # om gissining är h eller l så kommmeer det köra koden 1 om man kör exit så stängs programmet 
         continue
 
-    if guess == "exit":
+    if guess == "exit": ## koden som stänger programmet och skriver ut resultatet
         print("Din score blev:", score,"total")
         print("Tack för att du spelade")
         break
 
     new_num = random.randint(min, max)
-
+    #kod1 som jag kallar det här körs det om man väljer h eller l och det forstätters tills man skriver exit för att avslutat
     if new_num >= random_num and guess == "h":
         score += 1
         print("Rätt, din score är: ", score)
