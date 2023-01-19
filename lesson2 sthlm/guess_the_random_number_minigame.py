5255bf13a9f0bba8e9101841aab06a6e74659a95
GNugger <wanfay.suen@gmail.com>
2023-01-19 13:57:02 +0100

    vet inte vad jag har gjort

--- lesson2 sthlm/guess_the_random_number_minigame.py
+++ lesson2 sthlm/guess_the_random_number_minigame.py
@@ -7,7 +7,7 @@ tries = 0
 random_num = random.randint(min, max)
 
 while True:
-    guess = input("Guess the number between 1-100 you have 10 tries. Or press 0 to exit the program ")
+    guess = input("Guess the number between 1-100 you have 10 tries. Or press 0 to exit the program: ")
     guess = int(guess)
     
     if guess == 0:
--- lesson3/lektion3.py
+++ lesson3/lektion3.py
@@ -1,16 +1,141 @@
+from typehints_typings import my_function as my_imported_funtion
+# Indentering
+# Funktioner
+# Definition
+# parametrar
+# argument (position, namngivna)
 
 
-def my_function(name):
-    print("in my function")
-    return "hej"+ name
+def my_function(name, surname, *rest):
+    print("In my function ", rest)
+    return "Hej " + name
 
-# my_function ("")
-#print(my_function("Fay"))
 
-def a_general_function(*args,**kwargs): # en asterisk det är postional arguments och två är keywords arguments
-        a,b,c,d,e,f,g=args # position spelar roll
-        print 
-        print(a,g)
-        print(args,kwargs)
-        
-a_general_function("a","b",1,2,3,True,False, name="Fay") ## kan inte lika keywe
+# my_function("Anton", "Appelblom", 1)
+# print(my_function("Anton"))
+
+
+def a_general_function(x, y, *args, is_employed=False, **kwargs): # en asterisk(*) är positional arguments och två(**) är keyword arguments     
+    # a, b, c, d, e, f, g = args
+    a, b, c, d, e = args
+    print(kwargs["name"])
+    print(is_employed)
+    # print(a, g)
+    print(a, e)
+    print(args, kwargs)
+
+
+# a_general_function("A", "B", 1, 2, 3, True, False, name="Anton")
+
+
+def generate_person(name, surname, *attributes, is_employed, hobbies, **kwargs):
+    print(name, surname, " is a person with ", attributes)
+    print(name, " is ")
+    if is_employed:
+        print("Employed")
+    else:
+        print("Unemployed")
+
+    print("Hobbies are:")
+    for hobby in hobbies:
+        print(hobby)
+
+    print("Other info")
+    for key, value in kwargs.items():
+        print(key, value)
+
+
+# generate_person("Anton", "Appelblom", "Patient",
+#                 "Weird", "Bearded", is_employed=True, hobbies=["Games", "climbing", "Music", "Code"], can_run_a_marathon=False, can_fly=False)
+
+# Function with no *
+
+
+def function_without_catchallargs(pos1=None, pos2=None, kw1=None, kw2=None, **kwargs):
+    print(pos1, pos2, kw1, kw2, kwargs)
+    pass
+
+
+# function_without_catchallargs(pos1="A", pos2="B", kw1="HEj", kw2="HEj då")
+# function_without_catchallargs("A", "B", kw1="HEj", kw2="HEj då")
+# function_without_catchallargs("A", "B", "HEj", "HEj då")
+
+# Function with no **
+
+
+def function_without_catchallkwargs(pos1, pos2=None, kw1=None, kw2=None):
+    print(pos1, pos2, kw1, kw2)
+    pass
+
+
+# function_without_catchallkwargs(pos1="Sven", kw2="kalle")
+# function_without_catchallkwargs(kw2="kalle")
+
+def a_function_that_returns_an_int():
+    return 1
+
+
+def a_function_that_returns_an_string():
+    return "hej jag heter Anton"
+
+
+# print(a_function_that_returns_an_int(), type(a_function_that_returns_an_int()))
+# print(a_function_that_returns_an_string(),
+#       type(a_function_that_returns_an_string()))
+
+def a_function_that_returns_an_list(a, b, c):
+    return [a, b, c]
+
+
+my_list = a_function_that_returns_an_list(1, 2, 3)
+# print(my_list)
+# print(a_function_that_returns_an_list("Hej", "På", "Dig"))
+
+
+def a_function_that_returns_an_tuple(a, b, c):
+    return (a, b, c)
+
+
+# my_tuple = a_function_that_returns_an_tuple("A", "B", "C")
+value1, value2, value3 = a_function_that_returns_an_tuple("A", "B", "C")
+# print(my_tuple)
+# print(value1)
+# print(value3)
+
+
+def add_value(value1, value2, *args):
+    result = value1 + value2
+    for value in args:
+        result += value
+
+    return result
+
+
+my_result = add_value(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
+my_result2 = add_value("a", "b", "c", "d")
+# print(my_result)
+# print(my_result2)
+
+# Funktioner är i sig också objekt,
+# vilket betyder att en funktion kan skickas till en funktion som argument
+
+
+class MyClass():
+    a: int
+
+    def __init__(self):
+        print("Halloj från klass")
+
+
+def func1():
+    print("func1")
+
+
+def func2(a):
+    a()
+    print(a)
+
+
+func2(MyClass)
+
+# my_imported_funtion()
